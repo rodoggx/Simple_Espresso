e0e9a7f23e227e35023e1826513c68190c2c3ccc
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-19 00:46:35 -0400

    Set RecyclerView adapter and layout manager

--- app/src/main/java/com/example/simplerecyclerview/MainActivity.java
+++ app/src/main/java/com/example/simplerecyclerview/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.simplerecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -10,11 +11,24 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
 
+    private ArrayList<String> mArrayList;
+    private SimpleAdapter mSimpleAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mArrayList = new ArrayList<>();
+        mSimpleAdapter = new SimpleAdapter(mArrayList);
+
         mRecyclerView = (RecyclerView) findViewById(R.id.a_main_recycler);
+        mRecyclerView.setAdapter(mSimpleAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        mArrayList.add("Hello");
+        mArrayList.add("World");
+        mArrayList.add("Cruel");
+        mSimpleAdapter.notifyDataSetChanged();
     }
 }
