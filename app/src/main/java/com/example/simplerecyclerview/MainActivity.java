5686e488f81c96552bb9e8ad961c12d36b676ccb
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-19 00:36:12 -0400

    Add RecyclerView in layout

--- app/src/main/java/com/example/simplerecyclerview/MainActivity.java
+++ app/src/main/java/com/example/simplerecyclerview/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.simplerecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.a_main_recycler);
     }
 }
